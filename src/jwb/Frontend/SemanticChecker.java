9ae2e064a97b165552352ed15dd20665b9004bc7
935963004 <38138106+935963004@users.noreply.github.com>
2019-03-26 17:24:35 +0800

    Update SemanticChecker.java

--- src/jwb/Frontend/SemanticChecker.java
+++ src/jwb/Frontend/SemanticChecker.java
@@ -171,7 +171,7 @@ public class SemanticChecker extends ScopeBuilder
     private boolean checkReturn(Type returnType)
     {
         if (returnType instanceof NullType) return !(currentReturnType instanceof ArrayType || currentReturnType instanceof ClassType);
-        if (returnType.equals(currentReturnType)) return false;
+        if (!returnType.equals(currentReturnType)) return false;
         return true;
     }
 
