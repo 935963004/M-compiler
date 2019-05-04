8c2b273596bcb0eba4270e7c5780b188d7ae7afe
935963004 <935963004@qq.com>
2019-05-04 19:10:21 +0800

    好像知道我说谎

--- codegen.bash
+++ codegen.bash
@@ -5,6 +5,4 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin Main"
-cat > program.txt   # save everything in stdin to program.txt
-$CCHK
+java -classpath lib/antlr-4.7.2-complete.jar:bin Main
